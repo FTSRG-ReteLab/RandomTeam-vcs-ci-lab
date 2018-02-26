f637de7935db08281bb99e6c2402218e6855e256
hanusch <hanusch.robi@gmail.com>
2018-02-26 09:00:12 -0800

    tachograph test added

--- tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
+++ tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -1,5 +1,46 @@
+package hu.bme.mit.train.tachograph;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+import javafx.geometry.Pos;
+
+import java.util.Random;
 
 public class Tachograph {
 
-    Table
+    private class Position{
+        private int x,y;
+
+        Position(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+
+    Table<Long,Position,Double> tacho;
+
+    Tachograph(){
+        tacho = HashBasedTable.create();
+    }
+
+    public void AddElement(Double speed){
+        Random random = new Random();
+        tacho.put(System.currentTimeMillis(), new Position(random.nextInt(),random.nextInt()),speed);
+    }
 }
---
+++ tachograph/src/test/java/hu/bme/mit/train/tachograph/TachographTest.groovy
@@ -0,0 +1,7 @@
+package hu.bme.mit.train.tachograph
+
+/**
+ * Created by meres on 2/26/18.
+ */
+class TachographTest {
+}
